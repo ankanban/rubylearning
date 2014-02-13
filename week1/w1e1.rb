56841ad1d62811b06924167e413f0c7b00b2be42
Anshuman P.Kanetkar <akanetkar@acm.org>
2014-02-13 19:03:31 +0000

    moved w1e3 into its correct pos

--- week1/w1e1.rb
+++ week1/w1e1.rb
@@ -1,56 +1,28 @@
+Exercise 1:
 
-def is_leap(year)
-	if (year % 400) == 0
-		return true
-	elsif (year % 100) == 0
-		return false
-	elsif (year % 4) == 0
-		return true
-	end
-	return false
-end
+I expected the output:
+true
+false
+true
 
-def how_old(seconds)
-	# Iterate back through the years, and
-	# for each year, reduce days appropriately,
-	# checking if it is a leap year.
+However, the actual output was
+false
+false
+true
 
-	num_days = seconds / (60 * 60 * 24.0)
-	year = Time.now.year
-	days_left = num_days
-	age = 0
-	age_frac = 0
-	while days_left > 0
-		days_in_yr = 365
-		if is_leap(year)
-			days_in_yr = 366
-		end
-		# Force floating point division
-		age_frac = days_left / days_in_yr.to_f
-		days_left -= days_in_yr
-		if days_left > 0
-			age += 1
-		end
-		year -= 1
-	end
-	age = age + age_frac
-	return age 
-end
+So the operator precendence in Ruby is clearly different.
 
-def how_old_avg(seconds)
-	# Use avg. days per year
-	return seconds / (60 * 60 * 24 * 365.2425)
-end
+On doing some further reading, I discovered that the 'or' and '||' operators 
+are identical except for precedence.
+'or' has much lower precedence than '||'.
+Apparently the 'or' operator is not meant for logical comparison, but is
+used mostly as a control flow operator for statements such as:
 
-def how_old_simple(seconds)
-	# No leap year factoring.
-	return seconds / (60 * 60 * 24 * 365.0)
-end
+<EXPRESSION1> or <EXPRESSION2> 
 
-age_in_seconds = 970000000
-puts format("%.10f", how_old(age_in_seconds))
-puts format("%.10f", how_old_avg(age_in_seconds))
-puts format("%.10f", how_old_simple(age_in_seconds))
-puts format("%.2f", how_old(age_in_seconds))
-puts format("%.2f", how_old_avg(age_in_seconds))
-puts format("%.2f", how_old_simple(age_in_seconds))
+where the 2 expressions have some important side-effects.
+Apparently this is a feature that was borrowed from Perl:
+
+open( FILE, "<filename>" ) or die "error"; 
+
+The above is a very common Perl idiom. 
